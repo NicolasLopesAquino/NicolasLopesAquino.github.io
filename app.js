a570550a4986d48694287d32baafe2e772cf4c5e
NicolasLopesAquino <40503283+NicolasLopesAquino@users.noreply.github.com>
2018-11-28 16:53:06 -0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,95 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+      const choices = ['r', 'p', 's'];
+      const randomNumber = Math.floor(Math.random() * 3);
+      return choices[randomNumber];
+}
+
+function convertText(text){
+  switch (text) {
+    case "p":
+      return "Papel";
+      break;
+
+    case "r":
+      return "Pedra";
+      break;
+
+    case "s":
+      return "Tesoura";
+      break;
+
+    default:
+    return " ";
+
+  }
+
+}
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_div.innerHTML = convertText(userChoice) + " venceu " + convertText(computerChoice) + ". Vc venceu!"
+  document.getElementById(userChoice).classlist.add(".green-glow");
+}
+
+function lose(userChoice, computerChoice) {
+  computerScore++;
+  computerScore_span.innerHTML = computerScore;
+  userScore_span.innerHTML = userScore;
+  result_div.innerHTML = convertText(computerChoice) + " venceu " + convertText(userChoice) + ". Vc perdeu!"
+}
+
+function draw(userChoice, computerChoice) {
+  computerScore_span.innerHTML = computerScore;
+  userScore_span.innerHTML = userScore;
+  result_div.innerHTML = convertText(computerChoice) + " empatou " + convertText(userChoice) + ". Empatado"
+}
+
+function game(userChoice){
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rp":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice, computerChoice);
+      break;
+  }
+
+}
+
+function main() {
+  rock_div.addEventListener('click', function() {
+    game("r");
+  })
+
+  paper_div.addEventListener('click', function() {
+    game("p")
+  })
+
+  scissors_div.addEventListener('click', function() {
+    game("s");
+  })
+}
+
+main();
